0aa69754bd3422536c885081ed63a69596b72deb
徐杨清 <xuyq@pingpongx.com>
2023-04-15 07:30:36 +0800

    feature: Demo support

--- src/main/java/com/ctech/gptdemo/controller/ChatController.java
+++ src/main/java/com/ctech/gptdemo/controller/ChatController.java
@@ -1,19 +1,23 @@
 package com.ctech.gptdemo.controller;
 
+import com.alibaba.fastjson2.JSON;
 import com.ctech.gptdemo.gpt.ChatGPTClient;
 import com.ctech.gptdemo.module.ChatRequest;
 import com.ctech.gptdemo.module.ChatRequestVo;
 import com.ctech.gptdemo.module.ChatResponse;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @RestController
 public class ChatController {
     private final ChatGPTClient gptClient;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChatController.class);
+
     @PostMapping("/sendMsg")
     ChatResponse sendMessage(@RequestBody ChatRequestVo msg){
+        LOGGER.info("request got."+ JSON.toJSONString(msg));
         try {
             return gptClient.sendMsg(msg.getMessage());
         } catch (Exception e) {
@@ -23,6 +27,7 @@ public class ChatController {
 
     @PostMapping("/sendMsgWithHistory")
     ChatResponse sendMessage(@RequestBody ChatRequest msg){
+        LOGGER.info("request got."+ JSON.toJSONString(msg));
         try {
             return gptClient.sendMsg(msg);
         } catch (Exception e) {
